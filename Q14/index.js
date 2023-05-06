5b701710788e7811ad7fd0b024e05f661264447b
Gerliano Souza <contatogerlianosouza@gmail.com>
2023-05-05 22:56:05 -0300

    atualização14

--- Q14/index.js
+++ Q14/index.js
@@ -1,35 +1,20 @@
-const nome = prompt("Digite o seu nome: ");
-const idade = prompt("Digite a sua idade: ");
-const celular = prompt("Digite o seu número de celular: ");
-const cidade = prompt("Digite a cidade onde mora: ");
 const usuario = {
-  nome: nome,
-  idade: idade,
-  celular: celular,
-  cidade: cidade
+  nome: prompt("Informe o seu nome: "),
+  idade: prompt("Informe a sua idade: "),
+  celular: prompt("Informe o seu número de celular: "),
+  cidade: prompt("Informe o município que você reside: "),
 };
-const alterar = confirm("Deseja alterar alguma informação?");
-if (alterar) {
+
+if (confirm("Deseja alterar alguma informação?")) {
   console.log("Informações atuais: ", usuario);
-  const alterar = prompt("Qual informação você deseja alterar? (nome, idade, celular ou cidade)");
-  switch (alterar) {
-    case "nome":
-      usuario.nome = prompt("Digite o novo nome: ");
-      break;
-    case "idade":
-      usuario.idade = prompt("Digite a nova idade: ");
-      break;
-    case "celular":
-      usuario.celular = prompt("Digite o novo número de celular: ");
-      break;
-    case "cidade":
-      usuario.cidade = prompt("Digite a nova cidade: ");
-      break;
-    default:
-      console.log("Opção inválida.");
-      break;
+  const opcoes = ["nome", "idade", "celular", "cidade"];
+  const escolha = prompt(`Qual informação você deseja alterar? (${opcoes.join(", ")})`);
+  if (opcoes.includes(escolha)) {
+    usuario[escolha] = prompt(`Digite o novo ${escolha}: `);
+    console.log("Novas informações: ", usuario);
+  } else {
+    console.log("Opção inválida.");
   }
-  console.log("Novas informações: ", usuario);
 } else {
   console.log("Informações do usuário: ", usuario);
 }
